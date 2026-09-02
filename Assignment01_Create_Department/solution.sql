d84c24a7b975758846c2a0610b97dd67a089e1f0
1U25CA090-SIVADA <skhannasivada@gmail.com>
2026-09-02 11:07:10 +0530

    Update solution.sql

--- Assignment01_Create_Department/solution.sql
+++ Assignment01_Create_Department/solution.sql
@@ -1,7 +1,4 @@
--- Write your SQL here
-
--- Create Database
-
--- Use Database
-
--- Create Department Table
+create database collegeDB1;
+use collegeDB1;
+create table department(DepartmentID int(5) primary key,DepartmentName varchar(20),HOD varchar(20));
+desc department;
